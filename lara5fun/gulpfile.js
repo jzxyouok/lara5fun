69310ea41f5754905b9b6cd246799988e535368b
ihuangmx <ihuangmx@qq.com>
2017-03-25 22:02:30 +0800

    bug fix

--- lara5fun/gulpfile.js
+++ lara5fun/gulpfile.js
@@ -2,15 +2,15 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
 
-    // mix.less('app.less')
-    // 	.coffee('module.coffee');
+    mix.less('app.less')
+    	.coffee('module.coffee');
 
+    mix.phpUnit();
+    
      mix.styles([
         "vendor/normalize.css",
         "app.css"
     ],'public/output/final.css',"public/css");
-
-    // mix.phpUnit();
 
     mix.version("public/output/final.css");
 });
--- lara5fun/resources/views/app.blade.php
+++ lara5fun/resources/views/app.blade.php
@@ -6,7 +6,7 @@
 	<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css">
 	<script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
 	<script src="http://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="{{elixir("css/all.css")}}">
+	<link rel="stylesheet" href="{{ elixir('output/final.css') }}">
 </head>
 <body>
 	<div class="container">
